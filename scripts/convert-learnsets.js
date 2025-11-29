db6c3ce743580f40c34e4a97e877dad3452b8c78
Alexander Petros <contact@alexpetros.com>
2025-11-29 14:26:26 -0500

    Add data and conversion script

---
+++ scripts/convert-learnsets.js
@@ -0,0 +1,23 @@
+#!node
+
+import pokedex from './pokedex.json' with { type: 'json' }
+import learnsets from './learnsets.json' with { type: 'json' }
+
+// Learnset
+for (const mon in pokedex) {
+  const learnset = learnsets[mon]?.learnset
+  for (const move in learnset) {
+    console.log(`learns('${mon}', '${move}').`)
+  }
+}
+
+// Types
+for (const mon in pokedex) {
+  const types = pokedex[mon]?.types
+  if (!types) { continue }
+  for (const type of types) {
+    console.log(`type('${mon}', ${type}).`)
+  }
+}
+
+
