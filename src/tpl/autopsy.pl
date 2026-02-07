7aca55492f205c7e526ef5c94d7b12b76f22460b
Alexander Petros <contact@alexpetros.com>
2026-02-07 18:50:35 -0500

    Support switches and moves

--- src/tpl/autopsy.pl
+++ src/tpl/autopsy.pl
@@ -1,5 +1,6 @@
 % https://github.com/smogon/pokemon-showdown/blob/df367633bce4d5d20516da8a98e648c508b3767f/sim/SIM-PROTOCOL.md
 :- use_module(library(lists)).
+:- use_module(library(os)).
 :- use_module(library(dif)).
 :- use_module(library(debug)).
 :- use_module(library(pio)).
@@ -8,32 +9,54 @@
 :- use_module(library(http/http_open)).
 
 run :-
-  phrase_from_file(log_lines(Ls), "./logs/gen9natdexdraft-2501138619.log"),
-  phrase_to_stream(print_logs(Ls), user_output).
+  argv([Fp|_]),
+  phrase_from_file(log_lines(Ls), Fp),
+  phrase_to_stream(print_battle(Ls), user_output).
 
-print_logs([L|Ls]) --> format_("~q~n", [L]), print_logs(Ls).
-print_logs([]) --> [].
+print_battle([A|As]) --> print_action(A), !, print_battle(As).
+print_battle([])     --> [].
+
+print_action(join(PHandle)) --> format_("~s joined~n", [PHandle]).
+print_action(switch(P, Nick, Mon, HP)) --> format_("Player ~d switched in ~s (~s) at ~s HP~n", [P, Nick, Mon, HP]).
+print_action(move(_, Nick, Move, _)) --> format_("~s used ~s~n", [Nick, Move]).
+print_action(_) --> [].
 
 log_lines([L|Ls]) --> log(L), log_lines(Ls).
-log_lines([]) --> [].
+log_lines([])     --> [].
 
-log(L) --> action(L), !.
+% Keep an eye out for a purer way to express this
+log(L)               --> action(L), !.
 log(u_action(A, Cs)) --> "|", rest(A), ("|", line(Cs) | "\n", { Cs = [] }).
 
-action(join(PHandle)) --> "|j|", line(PHandle).
-action(chat(PHandle, Message)) --> "|c|", rest(PHandle), "|", rest(Message), line(_).
-action(player(P, PHandle)) --> "|player|", rest(P), "|", rest(PHandle), line(_).
-action(poke(P, Mon)) --> "|poke|", rest(P), "|", rest(Mon), line(_).
-action(clearpoke) --> "|clearpoke\n".
-action(space) --> "|\n".
-action(teampreview) --> "|teampreview\n".
+% Meta
+action(join(PHandle))                 --> "|j|", line(PHandle).
+action(chat(PHandle, Message))        --> "|c|", rest(PHandle), "|", rest(Message), line(_).
+action(player(P, PHandle))            --> "|player|", player(P), "|", rest(PHandle), line(_).
+action(poke(P, Mon))                  --> "|poke|", rest(P), "|", rest(Mon), line(_).
+% Battle actions
+action(teampreview)                   --> "|teampreview\n".
+action(start)                         --> "|start\n".
+action(turn(Timestamp))               --> "|t:|", line(Timestamp).
+action(switch(P, Nick, Mon, HP))      --> "|switch|", battle_mon(P, Nick), "|", rest(Mon), "|", rest(HP), line(_).
+action(move(P, Nick, Move, Target))   --> "|move|", battle_mon(P, Nick), "|", rest(Move), "|", battle_mon(P, Nick), line(_).
+% Control
+action(clearpoke)                     --> "|clearpoke\n".
+action(space)                         --> "|\n".
+
+battle_mon(P, Nick) --> pos(P, _), ": ", rest(Nick).
+
+player(1) --> "p1".
+player(2) --> "p2".
+% More positions can be added to support doubles
+pos(1, a) --> "p1a".
+pos(2, a) --> "p2a".
 
 lines([])     --> call(eos), !.
 lines([L|Ls]) --> line(L), lines(Ls).
-line([])     --> ( "\n" | call(eos) ), !.
-line([C|Cs]) --> [C], line(Cs).
+line([])      --> ( "\n" | call(eos) ), !.
+line([C|Cs])  --> [C], line(Cs).
 
 rest([C|Cs]) --> [C], { [C] \= "|", [C] \= "\n" }, rest(Cs).
-rest([]) --> [].
+rest([])     --> [].
 
 eos([], []).
