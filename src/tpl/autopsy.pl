d66d2cde5f981ddfce9c032a103b016234a03cec
Alexander Petros <contact@alexpetros.com>
2026-02-07 19:33:50 -0500

    Add more predicates

--- src/tpl/autopsy.pl
+++ src/tpl/autopsy.pl
@@ -16,10 +16,15 @@ run :-
 print_battle([A|As]) --> print_action(A), !, print_battle(As).
 print_battle([])     --> [].
 
-print_action(join(PHandle)) --> format_("~s joined~n", [PHandle]).
-print_action(switch(P, Nick, Mon, HP)) --> format_("Player ~d switched in ~s (~s) at ~s HP~n", [P, Nick, Mon, HP]).
-print_action(move(_, Nick, Move, _)) --> format_("~s used ~s~n", [Nick, Move]).
-print_action(_) --> [].
+print_action(joined(PHandle))            --> format_("~s joined~n", [PHandle]).
+print_action(left(PHandle))              --> format_("~s left~n", [PHandle]).
+print_action(turn(T))                    --> format_("~nTurn ~d~n", [T]).
+print_action(move(_, Nick, Move, _))     --> format_("~s used ~s~n", [Nick, Move]).
+print_action(switch(P, Mon, Mon, HP))    --> format_("P~d switched in ~s at ~s HP~n", [P, Mon, HP]).
+print_action(switch(P, Nick, Mon, HP))   -->
+  { dif(Nick, Mon) },
+  format_("Player ~d switched in ~s (~s) at ~s HP~n", [P, Nick, Mon, HP]).
+print_action(_)                          --> [].
 
 log_lines([L|Ls]) --> log(L), log_lines(Ls).
 log_lines([])     --> [].
@@ -29,20 +34,25 @@ log(L)               --> action(L), !.
 log(u_action(A, Cs)) --> "|", rest(A), ("|", line(Cs) | "\n", { Cs = [] }).
 
 % Meta
-action(join(PHandle))                 --> "|j|", line(PHandle).
-action(chat(PHandle, Message))        --> "|c|", rest(PHandle), "|", rest(Message), line(_).
-action(player(P, PHandle))            --> "|player|", player(P), "|", rest(PHandle), line(_).
-action(poke(P, Mon))                  --> "|poke|", rest(P), "|", rest(Mon), line(_).
+action(joined(PHandle))                      --> "|j|", line(PHandle).
+action(left(PHandle))                        --> "|l|", line(PHandle).
+action(chat(PHandle, Message))               --> "|c|", rest(PHandle), "|", rest(Message), line(_).
+action(player(P, PHandle))                   --> "|player|", player(P), "|", rest(PHandle), line(_).
+action(poke(P, Mon))                         --> "|poke|", rest(P), "|", id_mon(Mon, _), line(_).
 % Battle actions
-action(teampreview)                   --> "|teampreview\n".
-action(start)                         --> "|start\n".
-action(turn(Timestamp))               --> "|t:|", line(Timestamp).
-action(switch(P, Nick, Mon, HP))      --> "|switch|", battle_mon(P, Nick), "|", rest(Mon), "|", rest(HP), line(_).
-action(move(P, Nick, Move, Target))   --> "|move|", battle_mon(P, Nick), "|", rest(Move), "|", battle_mon(P, Nick), line(_).
+action(teampreview)                          --> "|teampreview\n".
+action(start)                                --> "|start\n".
+action(turn(T))                              --> "|turn|", line(TS), { number_chars(T, TS) }.
+action(timestamp(Timestamp))                 --> "|t:|", line(Timestamp).
+action(switch(P, Nick, Mon, HP))             --> "|switch|", battle_mon(P, Nick), "|", id_mon(Mon, _), rest(HP), line(_).
+action(move(P, Nick, Move, _))               --> "|move|", battle_mon(P, Nick), "|", rest(Move), "|", battle_mon(_, _), line(_).
 % Control
-action(clearpoke)                     --> "|clearpoke\n".
-action(space)                         --> "|\n".
+action(clearpoke)                            --> "|clearpoke\n".
+action(space)                                --> "|\n".
 
+id_mon(Mon, Details) -->
+    (to_comma_or_sep(Mon), "|", { Details = [] })
+  | (to_comma_or_sep(Mon), ",", rest(Details), "|").
 battle_mon(P, Nick) --> pos(P, _), ": ", rest(Nick).
 
 player(1) --> "p1".
@@ -56,6 +66,9 @@ lines([L|Ls]) --> line(L), lines(Ls).
 line([])      --> ( "\n" | call(eos) ), !.
 line([C|Cs])  --> [C], line(Cs).
 
+
+to_comma_or_sep([C|Cs]) --> [C], { [C] \= "\n", [C] \= "," }, to_comma_or_sep(Cs).
+to_comma_or_sep([])     --> [].
 rest([C|Cs]) --> [C], { [C] \= "|", [C] \= "\n" }, rest(Cs).
 rest([])     --> [].
 
