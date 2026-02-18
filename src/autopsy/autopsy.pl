5b8945772eda01ccfb980e942621fa9c0475c603
Alexander Petros <contact@alexpetros.com>
2026-02-17 23:45:01 -0500

    Add miss percentage

--- src/autopsy/autopsy.pl
+++ src/autopsy/autopsy.pl
@@ -18,10 +18,6 @@ unknown(Fp) :- phrase_from_file(log_lines(Ls), Fp), phrase_to_stream(print_unkno
 
 load_log(Fp, Ls) :- phrase_from_file(log_lines(Ls), Fp).
 
-is_move(action(A, _), T) :- =(A, move, T).
-missed_move(action(move, [_, _, _, Res]), T) :- =(Res, miss, T).
-extract_move(action(move, [_, Move, _, _]), Move).
-
 % With a lot of help from:
 % https://stackoverflow.com/questions/10776759/how-to-count-number-of-element-occurrences-in-a-list-in-prolog
 % count_and_remove/5 builds a new list without the Target, while counting the occurences of Target
@@ -32,27 +28,39 @@ count_and_remove([E|Es], Target, Ls0, N0, N) :-
     (Ls0 = Ls, N1 #= N0 + 1)
   ),
   count_and_remove(Es, Target, Ls, N1, N).
-
 % count/2 relates a list to a list of E-N terms, where N is the number of occurences of E
 count([], []).
 count([E|Es], [E-N|Ls]) :-
-  count_item(Es, E, Es0, 1, N),
+  count_and_remove(Es, E, Es0, 1, N),
   count(Es0, Ls).
 
-% count_([E|Es], Ls0, Ls) :-
-%   if_(
-%     memberd_t(E-C0, Ls0),
-%     ( select(E-C0, Ls0, Ls1), C #= C0 + 1, append(Ls1, [E-C], Ls2) ),
-%     append(Ls0, [E-1], Ls2)
-%   ),
-%   count_(Es, Ls2, Ls).
+is_move(action(A, _), T) :- =(A, move, T).
+hit_or_missed_move(action(move, [_, _, _, Res]), T) :- memberd_t(Res, [none, miss], T).
+missed_move(action(move, [_, _, _, Res]), T) :- =(Res, miss, T).
+extract_move(action(move, [_, Move, _, _]), Move).
 
-moves(Fp, Moves) :-
+moves_count(Fp, Moves) :-
+  phrase_from_file(log_lines(Ls), Fp),
+  tfilter(is_move, Ls, Actions),
+  tfilter(hit_or_missed_move, Actions, MissedMoves),
+  maplist(extract_move, MissedMoves, MovesUncounted),
+  count(MovesUncounted, Moves).
+
+missed_moves_count(Fp, Moves) :-
   phrase_from_file(log_lines(Ls), Fp),
   tfilter(is_move, Ls, Actions),
   tfilter(missed_move, Actions, MissedMoves),
-  maplist(extract_move, MissedMoves, Moves)
-  .
+  maplist(extract_move, MissedMoves, MovesUncounted),
+  count(MovesUncounted, Moves).
+
+miss_pct(UsedMoves, Move-Misses, Move-Pct) :-
+  select(Move-Usages, UsedMoves, _),
+  Pct is 1 - (Misses / Usages).
+
+missed_moves_pct(Fp, Moves) :-
+  moves_count(Fp, UsedMoves),
+  missed_moves_count(Fp, MissedMoves),
+  maplist(miss_pct(UsedMoves), MissedMoves, Moves).
 
 move(Fp, Move, Result) :-
   phrase_from_file(log_lines(Ls), Fp),
